721eb90975b08a2c6e5bc2340a5799a550ca3633
Darwin Xu <darwin.xu@icloud.com>
2025-11-22 14:04:09 +0800

    Add more test cases

--- src/main.ts
+++ src/main.ts
@@ -82,6 +82,115 @@ const TEST_CASES: TestCase[] = [
         A: { x: 140, y: 20, width: 100, height: 60 },
         B: { x: 20, y: 10, width: 100, height: 60 },
     },
+    {
+        A: { x: 0, y: 0, width: 100, height: 40 },
+        B: { x: 105, y: 0, width: 100, height: 40 },
+    },
+    {
+        A: { x: 0, y: 0, width: 100, height: 40 },
+        B: { x: 100, y: 0, width: 100, height: 40 },
+    },
+    {
+        A: { x: 0, y: 0, width: 100, height: 40 },
+        B: { x: 80, y: 0, width: 100, height: 40 },
+    },
+    {
+        A: { x: 0, y: 0, width: 200, height: 40 },
+        B: { x: 80, y: 0, width: 60, height: 40 },
+    },
+
+    {
+        A: { x: 0, y: 0, width: 120, height: 60 },
+        B: { x: 0, y: 80, width: 120, height: 60 },
+    },
+    {
+        A: { x: 50, y: 100, width: 100, height: 40 },
+        B: { x: 50, y: 40, width: 100, height: 40 },
+    },
+    {
+        A: { x: 100, y: 100, width: 100, height: 40 },
+        B: { x: 110, y: 220, width: 100, height: 40 },
+    },
+    {
+        A: { x: 200, y: 0, width: 80, height: 40 },
+        B: { x: 220, y: 600, width: 80, height: 40 },
+    },
+
+    {
+        A: { x: 0, y: 0, width: 150, height: 80 },
+        B: { x: 200, y: 20, width: 150, height: 80 },
+    },
+    {
+        A: { x: 0, y: 0, width: 150, height: 80 },
+        B: { x: 100, y: 40, width: 150, height: 80 },
+    },
+    {
+        A: { x: 0, y: 120, width: 150, height: 80 },
+        B: { x: 100, y: 40, width: 150, height: 80 },
+    },
+    {
+        A: { x: 100, y: 100, width: 120, height: 60 },
+        B: { x: 180, y: 60, width: 120, height: 140 },
+    },
+
+    {
+        A: { x: 200, y: 200, width: 100, height: 40 },
+        B: { x: 50, y: 100, width: 100, height: 40 },
+    },
+    {
+        A: { x: 200, y: 50, width: 120, height: 60 },
+        B: { x: 40, y: 220, width: 120, height: 60 },
+    },
+    {
+        A: { x: 200, y: 0, width: 120, height: 40 },
+        B: { x: 40, y: 0, width: 120, height: 40 },
+    },
+
+    {
+        A: { x: 100, y: 100, width: 200, height: 120 },
+        B: { x: 140, y: 130, width: 80, height: 60 },
+    },
+    {
+        A: { x: 140, y: 130, width: 80, height: 60 },
+        B: { x: 100, y: 100, width: 200, height: 120 },
+    },
+    {
+        A: { x: 100, y: 100, width: 120, height: 60 },
+        B: { x: 100, y: 100, width: 120, height: 60 },
+    },
+
+    {
+        A: { x: 0, y: 0, width: 100, height: 40 },
+        B: { x: 2000, y: 50, width: 100, height: 40 },
+    },
+    {
+        A: { x: -200, y: -100, width: 120, height: 60 },
+        B: { x: 150, y: 80, width: 120, height: 60 },
+    },
+    {
+        A: { x: 100, y: 0, width: 100, height: 40 },
+        B: { x: 0, y: 40, width: 100, height: 40 },
+    },
+    {
+        A: { x: 100, y: 0, width: 100, height: 40 },
+        B: { x: 0, y: 41, width: 100, height: 40 },
+    },
+    {
+        A: { x: 100, y: 0, width: 100, height: 40 },
+        B: { x: 0, y: 42, width: 100, height: 40 },
+    },
+    {
+        A: { x: 100, y: 0, width: 100, height: 40 },
+        B: { x: 0, y: 43, width: 100, height: 40 },
+    },
+    {
+        A: { x: 100, y: 0, width: 100, height: 40 },
+        B: { x: 0, y: 50, width: 100, height: 40 },
+    },
+    {
+        A: { x: 100, y: 0, width: 100, height: 40 },
+        B: { x: 0, y: 60, width: 100, height: 40 },
+    },
 ];
 
 // Basic padding around drawing
